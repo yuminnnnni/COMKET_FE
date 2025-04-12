5361ab90c04e9a6f3a94a5e4e604a3d946875ad4
yuminnnnni <ka09023@ajou.ac.kr>
2025-04-12 19:35:08 +0900

    chore: 프로젝트 초기 세팅 및 Vite 기반 환경 구성

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+/** @type {import('prettier').Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 100,
+  tabWidth: 2,
+};
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'COMKET',
+      short_name: 'COMKET',
+      description: ' 프로젝트 관리와 논의 경험을 동시에 연결하는 협업 플랫폼',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+  server: {
+    port: 3333,
+  },
+  preview: {
+    port: 3434,
+  },
+})
