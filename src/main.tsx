e4564777422c0715928663096576b960d360b882
yuminnnnni <ka09023@ajou.ac.kr>
2025-05-07 16:47:11 +0000

    chore: 로컬/배포 환경 구분 위한 redirect URI 로그 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+console.log("🔧 BACKEND_URL:", import.meta.env.VITE_BACKEND_URL)
+console.log("🔧 REDIRECT_URI:", import.meta.env.VITE_GOOGLE_AUTH_REDIRECT_URI)
+
 import { StrictMode } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
